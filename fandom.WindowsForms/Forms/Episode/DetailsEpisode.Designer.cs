378d9d429a524a3325a2bb123cf177f1fa97d06e
zinedinmezit <zinedin.mezit@edu.fit.ba>
2020-11-13 17:30:53 +0100

    Episode edit/cancel UI manipulation

--- fandom.WindowsForms/Forms/Episode/DetailsEpisode.Designer.cs
+++ fandom.WindowsForms/Forms/Episode/DetailsEpisode.Designer.cs
@@ -29,30 +29,29 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DetailsEpisode));
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.eSeasonNumber = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             this.deleteButton = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.videoUrlTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(11, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(167, 26);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Episode title";
             // 
             // label2
             // 
@@ -86,15 +85,6 @@
             this.label3.Size = new System.Drawing.Size(139, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Air date 01-01-2000";
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 392);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(536, 59);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Some random text summary that has no purpose";
             // 
             // axWindowsMediaPlayer1
             // 
@@ -147,26 +137,166 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(598, 41);
+            this.label5.Location = new System.Drawing.Point(593, 41);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(129, 18);
             this.label5.TabIndex = 19;
             this.label5.Text = "Main characters";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(538, 506);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 33);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "EDIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(572, 41);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 18);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Select main characters";
+            this.label6.Visible = false;
+            // 
+            // listView2
+            // 
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView2.FullRowSelect = true;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(560, 62);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(204, 209);
+            this.listView2.TabIndex = 23;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.Visible = false;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Width = 0;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Character name";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 200;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(71, 360);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 24;
+            this.dateTimePicker1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.textBox2.Location = new System.Drawing.Point(16, 396);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(536, 79);
+            this.textBox2.TabIndex = 25;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(16, 8);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(533, 27);
+            this.textBox1.TabIndex = 26;
+            this.textBox1.Text = "Episode name";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(184, 506);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 33);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "SAVE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(300, 506);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 33);
+            this.button3.TabIndex = 28;
+            this.button3.Text = "CANCEL";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(579, 287);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 18);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Insert new video URL";
+            this.label1.Visible = false;
+            // 
+            // videoUrlTextBox
+            // 
+            this.videoUrlTextBox.Location = new System.Drawing.Point(560, 307);
+            this.videoUrlTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.videoUrlTextBox.Name = "videoUrlTextBox";
+            this.videoUrlTextBox.Size = new System.Drawing.Size(204, 20);
+            this.videoUrlTextBox.TabIndex = 30;
+            this.videoUrlTextBox.Visible = false;
+            // 
             // DetailsEpisode
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(776, 551);
+            this.Controls.Add(this.videoUrlTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.axWindowsMediaPlayer1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.eSeasonNumber);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "DetailsEpisode";
             this.Text = "DetailsEpisode";
             this.Load += new System.EventHandler(this.DetailsEpisode_Load);
@@ -177,17 +307,26 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label eSeasonNumber;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox videoUrlTextBox;
     }
 }
--- fandom.WindowsForms/Forms/Episode/DetailsEpisode.cs
+++ fandom.WindowsForms/Forms/Episode/DetailsEpisode.cs
@@ -1,4 +1,5 @@
-﻿using fandom.Model.Models;
+﻿using fandom.Model;
+using fandom.Model.Models;
 using fandom.Model.Requests;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ namespace fandom.WindowsForms.Forms.Episode
     {
         private readonly int _eId;
         private readonly APIService _episodeService = new APIService("Episode");
+        private readonly APIService _characterApiService = new APIService("Character");
+
 
         public DetailsEpisode(int id)
         {
@@ -27,7 +30,10 @@ namespace fandom.WindowsForms.Forms.Episode
         private async void DetailsEpisode_Load(object sender, EventArgs e)
         {
             var result = await _episodeService.GetById<MEpisode>(_eId);
-            BindData(result);
+            var characters = await _characterApiService.Get<List<MCharacter>>();
+
+
+            BindData(result, characters);
         }
 
         private async void deleteButton_Click(object sender, EventArgs e)
@@ -36,7 +42,7 @@ namespace fandom.WindowsForms.Forms.Episode
         }
 
 
-        private void BindData(MEpisode episode)
+        private void BindData(MEpisode episode, List<MCharacter> characters)
         {
             foreach (var item in episode.Characters)
             {
@@ -46,10 +52,10 @@ namespace fandom.WindowsForms.Forms.Episode
                 this.listView1.Items.Add(itemm);
             }
 
-            label1.Text = $"{episode.Title}";
+            textBox1.Text = $"{episode.Title}";
             label2.Text = $"(Overall episode:{episode.OverallNumberOfEpisode})";
             label3.Text = $"Air date {episode.AirDate:dd-MM-yyyy}";
-            label4.Text = episode.Summary;
+            textBox2.Text = episode.Summary;
             if (episode.MediaFile != null)
             {
                 axWindowsMediaPlayer1.URL = episode.MediaFile.Path;
@@ -59,6 +65,14 @@ namespace fandom.WindowsForms.Forms.Episode
             if (episode.Season != null)
             {
                 eSeasonNumber.Text = $"S{episode.Season.OrdinalNumber}E{episode.SeasonEpisodeNumber}";
+            }
+
+            foreach (var item in characters)
+            {
+                ListViewItem itemm = new ListViewItem(item.Id.ToString());
+                itemm.SubItems.Add($"{item.FirstName} {item.LastName}");
+
+                this.listView2.Items.Add(itemm);
             }
         }
 
@@ -74,5 +88,38 @@ namespace fandom.WindowsForms.Forms.Episode
             MessageBox.Show($"Episode {episodeInfo.Title} is removed");
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            changeVisibilty(true);
+            this.textBox1.BackColor = Color.White;
+            this.textBox2.BackColor = Color.White;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            changeVisibilty(false);
+            this.textBox1.BackColor = Color.WhiteSmoke;
+            this.textBox2.BackColor = Color.WhiteSmoke;
+        }
+
+        private void changeVisibilty(bool flag)
+        {
+            this.textBox1.ReadOnly = !flag;
+            this.eSeasonNumber.Visible = !flag;
+            this.label2.Visible = !flag;
+            this.label5.Visible = !flag;
+            this.listView1.Visible = !flag;
+            this.textBox2.ReadOnly = !flag;
+            this.deleteButton.Visible = !flag;
+            this.button1.Visible = !flag;
+
+            this.label6.Visible = flag;
+            this.listView2.Visible = flag;
+            this.label1.Visible = flag;
+            this.videoUrlTextBox.Visible = flag;
+            this.dateTimePicker1.Visible = flag;
+            this.button2.Visible = flag;
+            this.button3.Visible = flag;
+        }
     }
 }
